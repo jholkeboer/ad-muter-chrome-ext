a2ac73227dc994153cf5a365120ac454f39596cb
jholkeboer <jholkeboer@users.noreply.github.com>
2016-12-23 17:09:24 -0800

    toggle recording with button

--- background.js
+++ background.js
@@ -4,26 +4,8 @@ var screenshot = {
 	data : '',
 
 	init : function() {
+        this.recording = false;
 		this.initEvents();
-	},
-	
-	saveScreenshot : function() {
-		var image = new Image();
-		image.onload = function() {
-			var canvas = screenshot.content;
-			canvas.width = image.width;
-			canvas.height = image.height;
-			var context = canvas.getContext("2d");
-			context.drawImage(image, 0, 0);
-
-			// save the image
-			var link = document.createElement('a');
-			link.download = "download.png";
-			link.href = screenshot.content.toDataURL();
-			link.click();
-			screenshot.data = '';
-		};
-		image.src = screenshot.data; 
 	},
 	
 	initEvents : function() {
@@ -34,21 +16,20 @@ var screenshot = {
 			}, function(data) {
 				screenshot.data = data;
 				
-				// send an alert message to webpage
 				chrome.tabs.query({
 					active : true,
 					currentWindow : true
 				}, function(tabs) {
-					chrome.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
+					chrome.tabs.sendMessage(tabs[0].id, {ready : "ready", recording: screenshot.recording}, function(response) {
 						if (response.download === "download") {
-							// screenshot.saveScreenshot();
-                            console.log(screenshot.data);
-                            
+                            screenshot.recording = response.recording;
+
                             // send image to server
                             var time = Date.now();
                             var xhr = new XMLHttpRequest();
                             xhr.open("POST", "http://localhost:5000/receive_image", true);
                             xhr.setRequestHeader('Content-Type', 'application/json');
+                            // xhr.onreadystatechange();
                             xhr.send(JSON.stringify({
                                 time: time,
                                 image: screenshot.data
--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-	
 	if (msg.ready === "ready") {
-		if (confirm('Do you want to capture the screen?')) {
-			sendResponse({download : "download"});
-		}
+        if (msg.recording === false) {
+            var user_decision = confirm('Do you want to turn recording on?');
+            sendResponse({download: "download", recording: user_decision});
+        } else if (msg.recording === true) {
+            var user_decision = confirm('Do you want to turn recording off?');
+            sendResponse({download: "download", recording: !user_decision});
+        } else {
+            sendResponse({download : "download", recording: null});
+        }
+
 	}
 		
 }); 
